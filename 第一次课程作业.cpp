18ff073e55f1b88cc3f347de069156572d4361d8
shigewei <825330394@qq.com>
2017-04-29 16:11:13 +0800

    “补充“m补充计算式子

--- 第一次课程作业.cpp
+++ 第一次课程作业.cpp
@@ -2,7 +2,8 @@
 #include<ctime>
 #include<stdlib.h>
 using namespace std;
-int sum,turn=0,answer,correct,rig=0,wro=0,flag=0,num[5],test0=0;    //answer，用于接收用户输入的答案，correct用于存储计算所得正确答案，flag用于检测生成式子是否符合要求，test0用于检测除数是否为0
+int sum,turn=0,answer,correct,rig=0,wro=0,flag=0;    //answer，用于接收用户输入的答案，correct用于存储计算所得正确答案，flag用于检测生成式子是否符合要求
+float num[5];
 char sig[4];
 void scan()            //用于和用户交互
 {
@@ -44,7 +45,7 @@ void randomOperation()    //生成随机符号
 	int choose,j;
 	for(j=1;j<=3;j++)
 	{
-		choose=rand()%3;
+		choose=rand()%4;
     	switch(choose)
 		{
           	case 0:sig[j]='+';break;
@@ -55,7 +56,7 @@ void randomOperation()    //生成随机符号
 	}
 }
 
-float jisuan(float num1,char sign1,int num2,char sign2,int num3)    //得出三个数字的计算结果
+float jisuan(float num1,char sign1,float num2,char sign2,float num3)    //得出三个数字的计算结果
 {
 	float result;
 	if(sign1=='+')
@@ -78,7 +79,7 @@ float jisuan(float num1,char sign1,int num2,char sign2,int num3)    //得出三个数
 		}
 		else
 		{
-			test0=1;
+			flag=1;
 		}
 	}
 	if(sign2=='+')
@@ -92,9 +93,9 @@ float jisuan(float num1,char sign1,int num2,char sign2,int num3)    //得出三个数
 	if(sign2=='*')
 	{
 		if(sign1=='+')
-			result=(num1+num2)*num3;
-		if(sign2=='-')
-			result=(num1-num2)*num3;
+			result=num1+(num2*num3);
+		else if(sign1=='-')
+			result=num1-(num2*num3);
 		else
 			result=result*num3;
 	}
@@ -103,21 +104,21 @@ float jisuan(float num1,char sign1,int num2,char sign2,int num3)    //得出三个数
 		if(num2!=0)
 		{
 			if(sign1=='+')
-				result=(num1+num2)/num3;
-			if(sign2=='-')
-				result=(num1-num2)/num3;
+				result=num1+(num2/num3);
+			else if(sign1=='-')
+				result=num1-(num2/num3);
 			else
 				result=result/num3;
 		}
 		else
 		{
-			test0=1;
+			flag=1;
 		}
 	}
 	return result;
 }
 
-void generateExpression(int type)     //输出随机式子，此部分有待补充
+void generateExpression(int type)     //输出随机式子
 {
     switch(type)                     //用一个随机数控制输出随机式子，与calculateResult() 配套使用
 	{
@@ -126,17 +127,18 @@ void generateExpression(int type)     //输出随机式子，此部分有待补充
 		case 2:cout<<num[1]<<sig[1]<<num[2]<<sig[2]<<'('<<num[3]<<sig[3]<<num[4]<<')'<<'=';break;		//1+2+(3+4)=
 		case 3:cout<<num[1]<<sig[1]<<'('<<num[2]<<sig[2]<<num[3]<<')'<<sig[3]<<num[4]<<'=';break;		//1+(2+3)+4=
 		case 4:cout<<'('<<num[1]<<sig[1]<<num[2]<<sig[2]<<num[3]<<')'<<sig[3]<<num[4]<<'=';break;		//(1+2+3)+4=
+		case 5:cout<<num[1]<<sig[1]<<'('<<num[2]<<sig[2]<<num[3]<<sig[3]<<num[4]<<')'<<'=';break;		//1+(2+3+4)=
 	}
 }
-void calculateResult(int type)      //计算生成的随机式子的结果，此部分有待补充
+void calculateResult(int type)      //计算生成的随机式子的结果
 {
-
+	float te,result;
+	float t[5];
+	int test,sign[5];
+	int k;
 	if(type==0)									//1+2+3+4=
 	{
 		sig[0]='*';
-		float t[5];
-		int test,sign[5];
-		int k;
 		t[1]=num[1];
 		for(k=0;k<=3;k++)
 		{
@@ -197,32 +199,30 @@ void calculateResult(int type)      //计算生成的随机式子的结果，此部分有待补充
 	}
 	if(type==1)						//(1+2)+3+4=
 	{
-		float t,result;
-		int test;
 		if(sig[1]=='+')
 		{
-			t=num[1]+num[2];
+			te=num[1]+num[2];
 		}
 		if(sig[1]=='-')
 		{
-			t=num[1]-num[2];
+			te=num[1]-num[2];
 		}
 		if(sig[1]=='*')
 		{
-			t=num[1]*num[2];
+			te=num[1]*num[2];
 		}
 		if(sig[1]=='/')
 		{
 			if(num[2]!=0)
 			{
-				t=num[1]/num[2];
+				te=num[1]/num[2];
 			}
 			else
-				test0=1;
+				flag=1;
 		}
-		if(test0!=0)
+		if(flag==0)
 		{
-			result=jisuan(t,sig[2],num[3],sig[3],num[4]);
+			result=jisuan(te,sig[2],num[3],sig[3],num[4]);
 			test=result;
 			if(test==result&&test>=0)
 			{
@@ -236,32 +236,30 @@ void calculateResult(int type)      //计算生成的随机式子的结果，此部分有待补充
 	}
 	if(type==2)						//1+2+(3+4)=
 	{
-		float t,result;
-		int test;
 		if(sig[3]=='+')
 		{
-			t=num[3]+num[4];
+			te=num[3]+num[4];
 		}
 		if(sig[3]=='-')
 		{
-			t=num[3]-num[4];
+			te=num[3]-num[4];
 		}
 		if(sig[3]=='*')
 		{
-			t=num[3]*num[4];
+			te=num[3]*num[4];
 		}
 		if(sig[3]=='/')
 		{
 			if(num[4]!=0)
 			{
-				t=num[3]/num[4];
+				te=num[3]/num[4];
 			}
 			else
-				test0=1;
+				flag=1;
 		}
-		if(test0!=0)
+		if(flag==0)
 		{
-			result=jisuan(num[1],sig[1],num[2],sig[2],t);
+			result=jisuan(num[1],sig[1],num[2],sig[2],te);
 			test=result;
 			if(test==result&&test>=0)
 			{
@@ -275,32 +273,30 @@ void calculateResult(int type)      //计算生成的随机式子的结果，此部分有待补充
 	}
 	if(type==3)						//1+(2+3)+4=
 	{
-		float t,result;
-		int test;
 		if(sig[2]=='+')
 		{
-			t=num[2]+num[3];
+			te=num[2]+num[3];
 		}
 		if(sig[2]=='-')
 		{
-			t=num[2]-num[3];
+			te=num[2]-num[3];
 		}
 		if(sig[2]=='*')
 		{
-			t=num[2]*num[3];
+			te=num[2]*num[3];
 		}
 		if(sig[2]=='/')
 		{
 			if(num[3]!=0)
 			{
-				t=num[2]/num[3];
+				te=num[2]/num[3];
 			}
 			else
-				test0=1;
+				flag=1;
 		}
-		if(test0!=0)
+		if(flag==0)
 		{
-			result=jisuan(num[1],sig[1],t,sig[3],num[4]);
+			result=jisuan(num[1],sig[1],te,sig[3],num[4]);
 			test=result;
 			if(test==result&&test>=0)
 			{
@@ -314,31 +310,67 @@ void calculateResult(int type)      //计算生成的随机式子的结果，此部分有待补充
 	}
 	if(type==4)						//(1+2+3)+4=
 	{
-		float t,result;
-		int test;
-		t=jisuan(num[1],sig[1],num[2],sig[2],num[3]);
+		te=jisuan(num[1],sig[1],num[2],sig[2],num[3]);
 		if(sig[3]=='+')
 		{
-			result=t+num[4];
+			result=te+num[4];
 		}
 		if(sig[3]=='-')
 		{
-			result=t-num[4];
+			result=te-num[4];
 		}
 		if(sig[3]=='*')
 		{
-			result=t*num[4];
+			result=te*num[4];
 		}
 		if(sig[3]=='/')
 		{
 			if(num[4]!=0)
 			{
-				result=t/num[4];
+				result=te/num[4];
 			}
 			else
-				test0=1;
+				flag=1;
 		}
-		if(test0!=0)
+		if(flag==0)
+		{
+	
+			test=result;
+			if(test==result&&test>=0)
+			{
+				correct=test;
+			}
+			else
+			{
+				flag=1;
+			}
+		}
+	}
+	if(type==5)						//1+(2+3+4)=
+	{
+		te=jisuan(num[2],sig[2],num[3],sig[3],num[4]);
+		if(sig[1]=='+')
+		{
+			result=num[1]+te;
+		}
+		if(sig[1]=='-')
+		{
+			result=num[1]-te;
+		}
+		if(sig[1]=='*')
+		{
+			result=num[1]*te;
+		}
+		if(sig[1]=='/')
+		{
+			if(t!=0)
+			{
+				result=num[1]/te;
+			}
+			else
+				flag=1;
+		}
+		if(flag==0)
 		{
 	
 			test=result;
@@ -371,7 +403,7 @@ int main()           //主函数
 	{
 		randomNumber();
 		randomOperation();
-		type=rand()%5;
+		type=rand()%6;
 		calculateResult(type);
 		if(flag==0)
 		{
@@ -381,11 +413,9 @@ int main()           //主函数
 		}
 		else
 		{
-			test0=0;
 			flag=0;
 			continue;
 		}
-		test0=0;
 		i++;
 	}
 	print();
